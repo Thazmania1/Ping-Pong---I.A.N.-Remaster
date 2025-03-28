6b3d40aaf8190f94221b47b24e4fb3357c6518c3
Thazmania1 <martinezpicazoian@gmail.com>
2025-03-28 11:11:27 +0100

    Simple ball movement (Very wonky physics)

---
+++ Assets/Scripts/Scripts_Match/BallBehaviour.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour
+{
+    // Declares a class level ball physics variable.
+    private Rigidbody2D ball_physics;
+
+    // Defines the ball's speeds and directions.
+    private float x_speed;
+    private float y_speed;
+    private int x_direction;
+    private int y_direction;
+
+    void Start()
+    {
+        ball_physics = GetComponent<Rigidbody2D>();
+
+        x_speed = 5f;
+        y_speed = 5f;
+        x_direction = (Random.Range(0, 2) * 2) - 1;
+        y_direction = (Random.Range(0, 2) * 2) - 1;
+    }
+
+    void Update()
+    {
+        // Dynamic speed calculation.
+        ball_physics.linearVelocity = new Vector2(x_speed * x_direction, y_speed * y_direction);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Checks what collided.
+        if (collision.gameObject.CompareTag("VerticalBorder"))
+        {
+            // Reverses the vertical direction of the ball.
+            y_direction *= -1;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("I gi in MF");
+        // Checks what was triggered.
+        if (collision.CompareTag("BallCollision"))
+        {
+            // Reverses the horitzontal direction of the ball.
+            x_direction *= -1;
+        }
+        else if (collision.CompareTag("Goal"))
+        {
+            // Resets the ball's position and randomizes its physics.
+            transform.position = new Vector3(0, 0, transform.position.z);
+            x_speed = 5f;
+            y_speed = 5f;
+            x_direction = (Random.Range(0, 2) * 2) - 1;
+            y_direction = (Random.Range(0, 2) * 2) - 1;
+        }
+    }
+}
--- Assets/Scripts/Scripts_Match/PlayerControls.cs
+++ Assets/Scripts/Scripts_Match/PlayerControls.cs
@@ -4,14 +4,19 @@ using UnityEngine.InputSystem;
 public class PlayerControls : MonoBehaviour
 {
     // Declares a class level player physics variable.
-
-    private Rigidbody2D physics;
+    private Rigidbody2D player_physics;
 
     // Defines the player's speed.
     private float speed = 5f;
+
     void Start()
     {
-        physics = GetComponent<Rigidbody2D>();
+        player_physics = GetComponent<Rigidbody2D>();
+
+        // Makes players' restrictive colliders not be interactable with the ball.
+        Collider2D player_collider = GetComponent<Collider2D>();
+        Collider2D ball_collider = GameObject.Find("Ball").GetComponent<Collider2D>();
+        // Physics2D.IgnoreCollision(player_collider, ball_collider, true);
     }
 
     void Update()
@@ -26,6 +31,6 @@ public class PlayerControls : MonoBehaviour
         float input = action.ReadValue<float>();
         
         // Sets the vertical movement velocity.
-        physics.linearVelocityY = input * speed;
+        player_physics.linearVelocityY = input * speed;
     }
 }
